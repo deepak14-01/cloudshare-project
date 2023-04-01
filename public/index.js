7fd1e1b0cdc116558736d4b56ee7479f3ec8b7f6
Dee Pak <49827857+deepak14-01@users.noreply.github.com>
2023-04-01 13:47:37 +0000

    Changed env variables

--- public/index.js
+++ public/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://improbablefarawaybuttons.deepak3.repl.co";
+const baseURL = "https://cloudshare-pvuj.onrender.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
